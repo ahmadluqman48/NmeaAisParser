2b3352362b8322f32f55fff9292452d0472b0477
OnikGein <OnikGein@DESKTOP-3H18DMT>
2026-06-27 01:05:10 +0800

    Fix mismatch between the listening address specified in Program.cs and the URLs in launchSettings

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 var app = builder.Build();
-builder.WebHost.UseUrls("http://0.0.0.0:5000");
+//builder.WebHost.UseUrls("http://0.0.0.0:5000");
 app.UseStaticFiles();
 app.UseRouting();
 app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
